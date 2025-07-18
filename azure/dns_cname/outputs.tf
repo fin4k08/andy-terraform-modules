19169b88b60b7b91ce7df5418577fc83c184e555
Andrew Findlay <fin4k08@live.co.uk>
2025-07-18 08:50:33 +0100

    adding more outputs to cname

--- azure/dns_cname/outputs.tf
+++ azure/dns_cname/outputs.tf
@@ -1,3 +1,11 @@
 output "fqdn" {
   value = azurerm_dns_cname_record.this.fqdn
 }
+
+output "name" {
+  value = azurerm_dns_cname_record.this.name
+}
+
+output "zone_name" {
+  value = azurerm_dns_cname_record.this.zone_name
+}
