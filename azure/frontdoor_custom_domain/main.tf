968dab8367e1e258c51db46d0ba9316c9b159f74
Andrew Findlay <fin4k08@live.co.uk>
2025-07-18 09:10:39 +0100

    fixing incorrect ref to var

--- azure/frontdoor_custom_domain/main.tf
+++ azure/frontdoor_custom_domain/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_cdn_frontdoor_custom_domain" "this" {
   name = var.name
-  cdn_frontdoor_profile_id = var.profile.id
+  cdn_frontdoor_profile_id = var.profile_id
   host_name = var.host_name
 
   tls {
