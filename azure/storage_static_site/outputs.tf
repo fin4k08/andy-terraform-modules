9863f0de94fdb618b2249bf6df9db95c31cf7752
Andrew Findlay <fin4k08@live.co.uk>
2025-07-20 08:45:32 +0100

    adding in clean output for web host for consuption into origins module

--- azure/storage_static_site/outputs.tf
+++ azure/storage_static_site/outputs.tf
@@ -7,3 +7,8 @@ output "primary_web_host" {
   description = "The static website endpoint"
   value       = module.storage.primary_web_host
 }
+
+output "primary_web_host_clean"{
+  description = "The static website without trailing slash"
+  value =  trimsuffix(module.storage.primary_web_host, "/")
+}
