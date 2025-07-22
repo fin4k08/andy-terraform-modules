25a162ac97cfe43bca71dcc3f4cb626e03b30142
Andrew Findlay <fin4k08@live.co.uk>
2025-07-22 08:05:26 +0100

    updating the output in the func app for stroage info

--- azure/function_app/outputs.tf
+++ azure/function_app/outputs.tf
@@ -9,3 +9,11 @@ output "function_app_id" {
 output "function_app_url" {
   value = azurerm_linux_function_app.this.default_hostname
 }
+
+output "storage_account_name" {
+  value = module.storage.storage_account_name
+}
+
+output "storage_account_access_key" {
+  value = module.storage.primary_access_key
+}
