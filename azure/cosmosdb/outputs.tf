4a8773aecbef588665195c93c4d16e6ca7bb59e6
Andrew Findlay <fin4k08@live.co.uk>
2025-07-23 08:21:18 +0100

    removing duplicate outputs cosmos

--- azure/cosmosdb/outputs.tf
+++ azure/cosmosdb/outputs.tf
@@ -21,11 +21,3 @@ output "endpoint" {
 output "primary_key" {
   value = azurerm_cosmosdb_account.this.primary_key
 }
-
-output "database_name" {
-  value = azurerm_cosmosdb_sql_database.this.name
-}
-
-output "container_name" {
-  value = azurerm_cosmosdb_sql_container.this.name
-}
