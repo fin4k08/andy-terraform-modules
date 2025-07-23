1fe4d49a4b21e1aceff1c84b859075647faa627a
Andrew Findlay <fin4k08@live.co.uk>
2025-07-23 08:34:39 +0100

    varibles updated for kv secrets

---
+++ azure/key_vault_secrets/variables.tf
@@ -0,0 +1,26 @@
+variable "key_vault_id" {
+  description = "ID of the target Azure Key Vault"
+  type        = string
+
+  validation {
+    condition     = length(var.key_vault_id) > 0
+    error_message = "Key Vault ID must not be empty."
+  }
+}
+
+variable "secrets" {
+  description = "Map of secrets to create (name = value)"
+  type        = map(string)
+
+  validation {
+    condition     = length(var.secrets) > 0
+    error_message = "You must provide at least one secret."
+  }
+
+  validation {
+    condition = alltrue([
+      for k, v in var.secrets : length(trim(k)) > 0 && length(trim(v)) > 0
+    ])
+    error_message = "Secret names and values cannot be empty."
+  }
+}
