d70ffd9f5d4f4d11de5d3fc5e478532f0627e78e
Andrew Findlay <fin4k08@live.co.uk>
2025-07-23 08:11:39 +0100

    updating outputs for cosmos

--- azure/cosmosdb/outputs.tf
+++ azure/cosmosdb/outputs.tf
@@ -13,3 +13,19 @@ output "container_name" {
 output "connection_string" {
   value = azurerm_cosmosdb_account.this.primary_sql_connection_string
 }
+
+output "endpoint" {
+  value = azurerm_cosmosdb_account.this.endpoint
+}
+
+output "primary_key" {
+  value = azurerm_cosmosdb_account.this.primary_key
+}
+
+output "database_name" {
+  value = azurerm_cosmosdb_sql_database.this.name
+}
+
+output "container_name" {
+  value = azurerm_cosmosdb_sql_container.this.name
+}
